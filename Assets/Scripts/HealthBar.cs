e18c767dc5cb3b11e229cb7443214472420e38d8
daviidlsu <dsando5@lsu.edu>
2024-10-16 14:04:03 -0500

    Update HealthBar.cs

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,17 +5,17 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-
+    // Public field for the slider component
     public Slider slider;
     
+    // To set the maxhealth for whatever sprite you want
     public void SetMaxHealth(int health){
         slider.maxValue = health;
         slider.value = health;
 
     }
-
+    // Updates the current health value of the slider
     public void SetHealth(int health){
-
         slider.value = health;
     }
 
