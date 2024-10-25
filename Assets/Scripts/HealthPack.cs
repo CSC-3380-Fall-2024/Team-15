d7bc9dbc358b0caa876813fff044c71bf53982e1
daviidlsu <dsando5@lsu.edu>
2024-10-25 13:40:43 -0500

    fixed the healthpack script and added regular player script

--- Assets/Scripts/HealthPack.cs
+++ Assets/Scripts/HealthPack.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class HealthPack : MonoBehaviour
 {
-    public GameObject pickupEffect;
+   
     public int multiplier = 15;
-    void OnTriggerEnter (Collider other)
+    void OnTriggerEnter2D (Collider2D other)
     {
+        Debug.Log("Triggered by: " + other.name);
         if (other.CompareTag("Player"))
         {
+            Debug.Log("player collided with pack");
             Pickup(other);
         }
     }
-    void Pickup(Collider player)
+    void Pickup(Collider2D player)
     {
-        Instantiate(pickupEffect, transform.position, transform.rotation);
-        HealthBar stats = player.GetComponent<HealthBar>();
-        stats.AddHealth(multiplier);
+       
+       Player playerScript = player.GetComponent<Player>();
+       playerScript.healthBar.AddHealth(multiplier);
         Destroy(gameObject);
     }
 }
