565180463431185bcad131515b060c38ab326019
AllinOne000 <alabra4@lsu.edu>
2024-10-30 10:30:33 -0500

    Revert "Attack Script, Health Script, and Hitbox Script (No Animations)"

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -13,30 +13,6 @@ public class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
         
-=======
-        if(Input.GetKeyDown(KeyCode.P))
-        {
-            PlayerAttack();
-        }
-
-        if(attacking)
-        {
-            timer += Time.deltaTime;
-            if(timer >= timeToAttack)
-            {
-                timer = 0;
-                attacking = false;
-                attackArea.SetActive(attacking);
-            }
-        }
-    }
-
-    private void PlayerAttack()
-    {
-        attacking = true;
-        attackArea.SetActive(attacking);
->>>>>>> Stashed changes
     }
 }
