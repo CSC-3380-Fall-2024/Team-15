af654e5d00e729ddf6e07bc9ebe77e6c39cdf12a
AllinOne000 <alabra4@lsu.edu>
2024-11-16 01:42:44 -0600

    Level Select Screen

---
+++ Assets/Scripts/LevelSelectManage.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectManage : MonoBehaviour
+{
+     public void level1()
+    {
+        //SceneManager.LoadScene("Level1");
+    }
+
+    public void level2()
+    {
+        //SceneManager.LoadScene("Level2");
+    }
+
+     public void level3()
+    {
+        //SceneManager.LoadScene("Level3");
+    }
+
+    public void level4()
+    {
+        //SceneManager.LoadScene("Level4");
+    }
+
+    public void menu()
+    {
+        //SceneManager.LoadScene("MainMenu");
+    }
+}
