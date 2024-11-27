2a0e226a328cb20bab8bb80fb57422e9e8e292f9
AllinOne000 <alabra4@lsu.edu>
2024-11-27 12:15:28 -0600

    Animations for Jab,ShortKick,Roundhouse,Uppercut, and changes to AttackScript code

--- Assets/Scripts/PlayerAttackArea.cs
+++ Assets/Scripts/PlayerAttackArea.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerAttackArea : MonoBehaviour
 {
     private int damage = 5;
-
+    
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.GetComponent<EnemyHealth>() != null)
