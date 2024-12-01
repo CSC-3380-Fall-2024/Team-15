7db74e075c0b1d856f8d59a085a29fac61f420de
hdaren2 <163777281+hdaren2@users.noreply.github.com>
2024-12-01 12:30:41 -0600

    Level 1 Setup

--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -9,7 +9,7 @@ using UnityEngine.Rendering;
 public class Slime : MonoBehaviour
 {
     private float moveSpeed = 2f;
-    public float chaseRange = 5f; 
+    public float chaseRange = 5f;
     public float attackRange = 1.5f;
     private int maxHealth = 50;
     private int currentHealth;
@@ -20,7 +20,7 @@ public class Slime : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     public Transform target;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,34 +33,35 @@ public class Slime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(player == null) return;
-            
-        if(currentHealth <= 0) Die();
+        if (player == null) return;
 
-          if(Input.GetKeyDown(KeyCode.U))
+        if (currentHealth <= 0) Die();
+
+        if (Input.GetKeyDown(KeyCode.U))
         {
             TakeDamage(10);
         }
-        float distanceToPlayer = Vector2.Distance(transform.position,player.position);
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
-        if(distanceToPlayer < chaseRange)
+        if (distanceToPlayer < chaseRange)
         {
             isChasing = true;
         }
-        else{
+        else
+        {
             isChasing = false;
         }
 
-        if(distanceToPlayer < attackRange && !isAttacking)
+        if (distanceToPlayer < attackRange && !isAttacking)
         {
             Attack();
         }
-        
+
         if (isChasing && !isAttacking)
         {
             MoveTowardPlayer();
         }
-        
+
         if (Vector3.Distance(transform.position, target.position) > 1f)
         {
             MoveTowardsTarget();
@@ -68,11 +69,11 @@ public class Slime : MonoBehaviour
 
         if (transform.position.x > player.position.x)
         {
-            transform.localScale = new Vector3(.4f, .4f, 1);
+            transform.localScale = new Vector3(.1f, .1f, 1);
         }
         else if (transform.position.x < player.position.x)
         {
-            transform.localScale = new Vector3(-.4f, .4f, 1);
+            transform.localScale = new Vector3(-.1f, .1f, 1);
         }
     }
 
@@ -80,19 +81,19 @@ public class Slime : MonoBehaviour
     {
         transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
     }
-     
+
     private void Attack()
     {
         Debug.Log("Attack");
         isAttacking = true;
         Health playerHealth = player.GetComponent<Health>();
-        if ( playerHealth != null)
+        if (playerHealth != null)
         {
             playerHealth.Damage(attackDamage);
         }
-      
+
         StartCoroutine(ResetAttack());
-    
+
     }
     private IEnumerator ResetAttack()
     {
@@ -123,8 +124,8 @@ public class Slime : MonoBehaviour
     {
         if (player != null)
         {
-             Vector2 direction = (player.position - transform.position).normalized; // Get direction
-             rb.velocity = new Vector2(direction.x * moveSpeed, rb.velocity.y);
+            Vector2 direction = (player.position - transform.position).normalized; // Get direction
+            rb.velocity = new Vector2(direction.x * moveSpeed, rb.velocity.y);
         }
     }
 
