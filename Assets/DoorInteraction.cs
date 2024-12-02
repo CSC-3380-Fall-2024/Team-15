a2fe33cc6b5765a76059e073715ce2774fe7ce01
daviidlsu <dsando5@lsu.edu>
2024-12-02 14:48:37 -0600

    Added boss scene encounter

---
+++ Assets/DoorInteraction.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorInteraction : MonoBehaviour
+{
+    private string bossFightSceneName = "BossFight";
+    private bool playerInRange = false;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.O))
+        {
+            LoadBossFight();
+        }
+    }
+
+    private void LoadBossFight()
+    {
+        SceneManager.LoadScene(bossFightSceneName);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            Debug.Log("Press 'E'to enter Boss fight");
+        }
+    }
+}
