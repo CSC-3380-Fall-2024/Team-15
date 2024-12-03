e2eb9adc02cbd722bf5a442289154d3629858092
hdaren2 <163777281+hdaren2@users.noreply.github.com>
2024-12-03 11:24:39 -0600

    Boss death

--- Assets/Scripts/BossHealth.cs
+++ Assets/Scripts/BossHealth.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossHealth : MonoBehaviour
 {
     [SerializeField] FloatingHealthbar healthBar;
     private int health = 100;
     public int MaxHealth = 100;
+    public GameManagerScript gameManager;
 
-    private void Start(){
+    private void Start()
+    {
         health = MaxHealth;
-        healthBar.UpdateHealthBar(health,MaxHealth);
+        healthBar.UpdateHealthBar(health, MaxHealth);
     }
     private void Update()
     {
@@ -19,10 +22,10 @@ public class BossHealth : MonoBehaviour
             Damage(10);
         }
     }
-    
+
     public void Damage(int amount)
     {
-        if(amount < 0)
+        if (amount < 0)
         {
             throw new System.ArgumentOutOfRangeException("Cannot have negative damage.");
         }
@@ -32,13 +35,14 @@ public class BossHealth : MonoBehaviour
         if (health <= 0)
         {
             Die();
+            SceneManager.LoadScene("LevelSelect");
         }
-        
+
     }
 
     public void Heal(int amount)
     {
-        if(amount < 0)
+        if (amount < 0)
         {
             throw new System.ArgumentOutOfRangeException("Cannot have negative healing.");
         }
@@ -53,19 +57,19 @@ public class BossHealth : MonoBehaviour
         {
             this.health += amount;
         }
-        
+
         health = Mathf.Min(health + amount, MaxHealth);
         healthBar.UpdateHealthBar(health, MaxHealth);
     }
 
-   private void Die()
-   {
-    Debug.Log("I am Dead!");
-    Destroy(gameObject);
-   }
+    private void Die()
+    {
+        Debug.Log("I am Dead!");
+        Destroy(gameObject);
+    }
 
-   private void Awake()
-   {
-    healthBar = GetComponentInChildren<FloatingHealthbar>();
-   }
+    private void Awake()
+    {
+        healthBar = GetComponentInChildren<FloatingHealthbar>();
+    }
 }
