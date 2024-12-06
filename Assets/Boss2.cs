271a0dce84c55613b5804ddd17add1d5871416d3
AllinOne000 <alabra4@lsu.edu>
2024-12-06 14:01:40 -0600

    Updates to BossArea

---
+++ Assets/Boss2.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2 : MonoBehaviour
+{
+    public float attackRange = 1.5f;
+    private Transform player;
+    private bool isAttacking = false;
+    public int attackDamage = 15;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null) return;
+
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < attackRange && !isAttacking)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack");
+        isAttacking = true;
+        Health playerHealth = player.GetComponent<Health>();
+        if (playerHealth != null)
+        {
+            playerHealth.Damage(attackDamage);
+        }
+
+        StartCoroutine(ResetAttack());
+
+    }
+    private IEnumerator ResetAttack()
+    {
+        yield return new WaitForSeconds(0.4f);
+        Debug.Log("resetting :)");
+        isAttacking = false;
+    }
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
