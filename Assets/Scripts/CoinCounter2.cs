88cb832b3fd02d5763e9793882d67227ba225d47
AllinOne000 <alabra4@lsu.edu>
2024-12-09 14:54:06 -0600

    Last Changes Before Making Builds

--- Assets/Scripts/CoinCounter2.cs
+++ Assets/Scripts/CoinCounter2.cs
@@ -17,7 +17,7 @@ public class CoinCounter2 : MonoBehaviour
     {
         coinText.text = coinCount.ToString();
 
-        if(coinCount == 20)
+        if(coinCount == 15)
         {
             LoadLevelSelect();
         }
